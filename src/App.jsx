3a119bd01b271202d5c2f878db833227c2d14261
githubscientist <sathish.datascientist@gmail.com>
2025-01-09 20:45:33 +0530

    layout setup done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-
-const routes = [
-  {
-    path: "/",
-    element: <h1>Hello World!!</h1>
-  }
-];
-
-const router = createBrowserRouter(routes, {
-  future: {
-    v7_relativeSplatPath: true,
-    v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true,
-    v7_partialHydration: true,
-    v7_skipActionErrorRevalidation: true,
-  },
-});
+import { Outlet } from "react-router";
+import Layout from "./layouts/Layout";
 
 const App = () => {
-
-  return <RouterProvider
-    router={router}
-    future={{
-      v7_startTransition: true,
-    }}
-  />
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  )
 }
 
 export default App;
---
+++ src/Router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+
+            }
+        ]
+    }
+];
+
+const router = createBrowserRouter(routes, {
+    future: {
+        v7_relativeSplatPath: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_skipActionErrorRevalidation: true,
+    },
+});
+
+export default router;
