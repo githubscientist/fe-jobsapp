5f2893e06c06a496332c7e6242fa6a487d9feab1
githubscientist <sathish.datascientist@gmail.com>
2025-01-10 18:30:51 +0530

    home page, register page, login page, navigation (layout) done

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
-import JobList from "./pages/JobList";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const routes = [
     {
         path: "/",
         element: <App />,
         children: [
-            {
-                path: "",
-                element: <JobList />
-            }
+            { path: "", element: <Home /> },
+            { path: "register", element: <Register /> },
+            { path: "login", element: <Login /> }
         ]
     }
 ];
